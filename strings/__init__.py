f62f8260508abf4e136c1c460fa927b918a5bed6
TheKerimov <125135848+TheKerimov@users.noreply.github.com>
2023-03-10 17:09:13 +0400

    Add files via upload

--- strings/__init__.py
+++ strings/__init__.py
@@ -8,16 +8,12 @@
 # All rights reserved.
 
 import os
-import sys
 from typing import List
 
 import yaml
 
 languages = {}
 commands = {}
-
-
-languages_present = {}
 
 
 def get_command(value: str) -> List:
@@ -35,29 +31,9 @@ for filename in os.listdir(r"./strings"):
             open(r"./strings/" + filename, encoding="utf8")
         )
 
-
 for filename in os.listdir(r"./strings/langs/"):
-    if "az" not in languages:
-        languages["az"] = yaml.safe_load(
-            open(r"./strings/langs/az.yml", encoding="utf8")
-        )
-        languages_present["az"] = languages["az"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "az":
-            continue
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["az"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["az"][item]
-    try:
-        languages_present[language_name] = languages[language_name][
-            "name"
-        ]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the TeamYukki at @YukkiSupport on Telegram"
-        )
-        sys.exit()
--- strings/helpers.py
+++ strings/helpers.py
@@ -8,27 +8,20 @@
 # All rights reserved.
 
 HELP_1 = """✅**<u>Admin Commands:</u>**
-
-**c** stands for channel play.
-
-/pause or /cpause - Pause the playing music.
-/resume or /cresume- Resume the paused music.
-/mute or /cmute- Mute the playing music.
-/unmute or /cunmute- Unmute the muted music.
-/skip or /cskip- Skip the current playing music.
-/stop or /cstop- Stop the playing music.
-/shuffle or /cshuffle- Randomly shuffles the queued playlist.
-/seek or /cseek - Forward Seek the music to your duration
-/seekback or /cseekback - Backward Seek the music to your duration
-/restart - Restart bot for your chat .
-
+/pause - Pause the playing music.
+/resume - Resume the paused music.
+/mute - Mute the playing music.
+/unmute - Unmute the muted music.
+/skip - Skip the current playing music.
+/stop - Stop the playing music.
+/shuffle - Randomly shuffles the queued playlist.
 
 ✅<u>**Specific Skip:**</u>
-/skip or /cskip [Number(example: 3)] 
+/skip [Number(example: 3)] 
     - Skips music to a the specified queued number. Example: /skip 3 will skip music to third queued music and will ignore 1 and 2 music in queue.
 
 ✅<u>**Loop Play:**</u>
-/loop or /cloop [enable/disable] or [Numbers between 1-10] 
+/loop [enable/disable] or [Numbers between 1-10] 
     - When activated, bot loops the current playing music to 1-10 times on voice chat. Default to 10 times.
 
 ✅<u>**Auth Users:**</u>
@@ -40,26 +33,33 @@ Auth Users can use admin commands without admin rights in your chat.
 
 
 HELP_2 = """✅<u>**Play Commands:**</u>
+/play or /vplay [Music Name or Youtube/Spotify/Apple/Resso/SoundCloud Link] 
+    - Bot will start playing your given query on voice chat.
 
-Available Commands = play , vplay , cplay
+/stream [m3u8 or index links] 
+    - Stream live links on voice chats.
 
-ForcePlay Commands = playforce , vplayforce , cplayforce
+/channelplay [Channel Username or ID] or [linked] 
+    - Connect channel to a group and stream music on channel's voice chat from your group. You need to be the **Owner** of the channel to connect it. Alternatively you can link your group to that channel and then try connnecting with `/channelplay linked`"
 
-**c** stands for channel play.
-**v** stands for video play.
-**force** stands for force play.
+After connecting channel, change playmode to channel from group via /playmode
 
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-
-/playforce or /vplayforce or /cplayforce -  **Force Play** stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-
-/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
-
+✅<u>**Supported Platform:**</u> 
+Bot only supports YouTube, AppleMusic, Spotify, Resso, Soundcloud, M3u8 and Index Links
 
 ✅**<u>Bot's Server Playlists:</u>**
 /playlist  - Check Your Saved Playlist On Servers.
 /deleteplaylist - Delete any saved music in your playlist
-/play  - Start playing Your Saved Playlist from Servers."""
+/play  - Start playing Your Saved Playlist from Servers.
+
+
+✅<u>**Play Settings:**</u>
+/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
+
+🔗 **Options in playmode:** [Get more info on clicking the button there]
+1️⃣ **Search Mode** [ Direct or Inline] :- Changes your search mode while you give /play mode. 
+2️⃣ **Play Mode** [ Group or Channel] :- Changes your Play mode to channel or group and streams music there only.
+3️⃣ **Play Type** [ Everyone or Admins] :- If admins, only admins present in group can play music on voice chat."""
 
 
 HELP_3 = """✅<u>**Bot Commands:**</u>
@@ -72,16 +72,13 @@ HELP_3 = """✅<u>**Bot Commands:**</u>
 
 /song [Track Name] or [YT Link] - Download any track from youtube in mp3 or mp4 formats.
 
-/player -  Get a interactive Playing Panel.
+/queue- Check Queue List of Music."""
 
-**c** stands for channel play.
-
-/queue or /cqueue- Check Queue List of Music."""
 
 HELP_4 = """✅<u>**Extra  Commands:**</u>
-/start - Start the Music Bot.
+/start - Start the Yukki Music Bot.
 /help  - Get Commands Helper Menu with detailed explanations of commands.
-/ping- Ping the Bot and check Ram, Cpu etc stats of Bot.
+/ping- Ping the Bot and check Ram, Cpu etc stats of Yukki.
 
 ✅<u>**Group Settings:**</u>
 /settings - Get a complete group's settings with inline buttons
@@ -94,82 +91,8 @@ HELP_4 = """✅<u>**Extra  Commands:**</u>
 
 3️⃣ **Auth Users**:- You can change admin commands mode from here to everyone or admins only. If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
 
-4️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
+4️⃣ **Play Mode Settings** :  Get help regarding in play commands section.
 
-5️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately.
+5️⃣ **Clean Mode:** When enabled deletes the bot's messages after 5 mins from your group to make sure your chat remains clean and good.
 
-6️⃣ **Play Settings:**
-
-/playmode - Get a complete play settings panel with buttons where you can set your group's play settings. 
-
-<u>Options in playmode:</u>
-
-1️⃣ **Search Mode** [Direct or Inline] - Changes your search mode while you give /play mode. 
-
-2️⃣ **Admin Commands** [Everyone or Admins] - If everyone, anyone present in you group will be able to use admin commands(like /skip, /stop etc)
-
-3️⃣ **Play Type** [Everyone or Admins] - If admins, only admins present in group can play music on voice chat."""
-
-HELP_5 = """🔰**<u>ADD & REMOVE SUDO USERS :</u>**
-/addsudo [Username or Reply to a user]
-/delsudo [Username or Reply to a user]
-
-🛃**<u>HEROKU:</u>**
-/usage - Dyno Usage.
-
-🌐**<u>CONFIG VARS:</u>**
-/get_var - Get a config var from Heroku or .env.
-/del_var - Delete any var on Heroku or .env.
-/set_var [Var Name] [Value] - Set a Var or Update a Var on heroku or .env. Seperate Var and its Value with a space.
-
-🤖**<u>BOT COMMANDS:</u>**
-/reboot - Reboot your Bot. 
-/update - Update Bot.
-/speedtest - Check server speeds
-/maintenance [enable / disable] 
-/logger [enable / disable] - Bot logs the searched queries in logger group.
-/get_log [Number of Lines] - Get log of your bot from heroku or vps. Works for both.
-/autoend [enable|disable] - Enable Auto stream end after 3 mins if no one is listening.
-
-📈**<u>STATS COMMANDS:</u>**
-/activevoice - Check active voice chats on bot.
-/activevideo - Check active video calls on bot.
-/stats - Check Bots Stats
-
-⚠️**<u>BLACKLIST CHAT FUNCTION:</u>**
-/blacklistchat [CHAT_ID] - Blacklist any chat from using Music Bot
-/whitelistchat [CHAT_ID] - Whitelist any blacklisted chat from using Music Bot
-/blacklistedchat - Check all blacklisted chats.
-
-👤**<u>BLOCKED FUNCTION:</u>**
-/block [Username or Reply to a user] - Prevents a user from using bot commands.
-/unblock [Username or Reply to a user] - Remove a user from Bot's Blocked List.
-/blockedusers - Check blocked Users Lists
-
-👤**<u>GBAN FUNCTION:</u>**
-/gban [Username or Reply to a user] - Gban a user from bot's served chat and stop him from using your bot.
-/ungban [Username or Reply to a user] - Remove a user from Bot's gbanned List and allow him for using your bot
-/gbannedusers - Check Gbanned Users Lists
-
-🎥**<u>VIDEOCALLS FUNCTION:</u>**
-/set_video_limit [Number of Chats] - Set a maximum Number of Chats allowed for Video Calls at a time. Default to 3 chats.
-/videomode [download|m3u8] - If download mode is enabled, Bot will download videos instead of playing them in M3u8 form. ByDefault to M3u8. You can use download mode when any query doesnt plays in m3u8 mode.
-
-⚡️**<u>PRIVATE BOT FUNCTION:</u>**
-/authorize [CHAT_ID] - Allow a chat for using your bot.
-/unauthorize [CHAT_ID] - Disallow a chat from using your bot.
-/authorized - Check all allowed chats of your bot.
-
-🌐**<u>BROADCAST FUNCTION:</u>**
-/broadcast [Message or Reply to a Message] - Broadcast any message to Bot's Served Chats.
-
-<u>options for broadcast:</u>
-**-pin** : This will pin your message 
-**-pinloud** : This will pin your message with loud notification
-**-user** : This will broadcast your message to the users who have started your bot.
-**-assistant** : This will broadcast your message from assistant account of your bot.
-**-nobot** : This will force your bot to not broadcast message
-
-**Example:** `/broadcast -user -assistant -pin Hello Testing`
-
-"""
+6️⃣ **Command Clean** : When activated, Bot will delete its executed commands (/play, /pause, /shuffle, /stop etc) immediately."""
