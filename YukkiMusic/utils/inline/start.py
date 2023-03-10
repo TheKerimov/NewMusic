16137fde7938423d47664866ebb5c96b34d9f9c2
TheKerimov <125135848+TheKerimov@users.noreply.github.com>
2023-03-10 23:19:04 +0400

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -79,41 +79,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                 ),
             ]
         )
-    else:
-        if SUPPORT_CHANNEL:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
-                    )
-                ]
-            )
-        if SUPPORT_GROUP:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
-                    )
-                ]
-            )
-    if GITHUB_REPO and OWNER:
-        buttons.append(
-            [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                ),
-            ]
-        )
-    else:
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                    ),
-                ]
-            )
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
