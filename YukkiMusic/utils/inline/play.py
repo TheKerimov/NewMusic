024c25db9102f32429cb30f3d71920c8eed90e1b
TheKerimov <125135848+TheKerimov@users.noreply.github.com>
2023-03-13 08:31:24 +0400

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -32,7 +32,6 @@ selections = [
 
 
 def stream_markup_timer(_, videoid, chat_id, played, dur):
-    bdef stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
@@ -90,6 +89,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
     ]
     return buttons
+
 
 
 
